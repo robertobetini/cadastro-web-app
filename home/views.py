ab04445369aebfde2d079129447b23ab9239e6dd
Roberto Betini Junior <55061623+robertobetini@users.noreply.github.com>
2020-11-21 20:06:27 -0300

    Add files via upload

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+
+# Create your views here.
+
+class Main(View):
+  def get(self, request):
+    return render(request, 'home.html')
